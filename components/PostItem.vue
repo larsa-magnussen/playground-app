6b5015875ace9b67912b5ddddede2f9e536d7cd2
lars <larsa.magnussen@gmail.com>
2024-12-17 14:50:28 +0100

    add db data to postitem, add props

--- components/PostItem.vue
+++ components/PostItem.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="post-container py-8">
         <h3 class="text-3xl font-bold">
-            <NuxtLink to="/posts/1">post title</NuxtLink>
+            <NuxtLink :to="`/posts/${post.id}`">
+                {{ post.title }}
+            </NuxtLink>
         </h3>
-        <div class="post-meta text-gray-700 flex items-center space-x-2">
-            <div>date</div>
+        <div class="post-meta font-semibold text-purple-200 flex items-center space-x-2">
+            <div>{{ formatedDate }}</div>
             <div>&middot;</div>
-            <div>name</div>
+            <div>{{ post.user.name }}</div>
         </div>
-        <div class="post-preview leading-relaxed mt-4">
-            haha cool text
+        <div class="post-preview leading-relaxed line-clamp-3 mt-4">
+            {{ post.body }}
         </div>
         <div class="mt-4">
             <Button
@@ -19,3 +21,14 @@
         </div>
     </div>
 </template>
+
+<script setup>
+    const props = defineProps({
+        post: {
+            type: Object,
+            required: true,
+        },
+    });
+
+    var formatedDate = new Date(props.post.created_at).toLocaleDateString('no-NO');
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
-        <div class="container mx-auto w-1/2 py-8 divide-y space-y-8 divide-gray-300">
-            <PostItem v-for="n in 10" :key="n" />
+        <div class="container mx-auto w-1/2 py-8 divide-y divide-gray-300">
+            <PostItem
+                v-for="post in posts"
+                :post="post"
+                :key="post.id"
+            />
         </div>
     </div>
 </template>
 
 <script setup>
+    const posts = ref([]);
+
+    onMounted(() => {
+        fetch('http://localhost/api/posts')
+            .then(response => response.json())
+            .then(data => posts.value = data);
+    });
+    console.log(posts);
 
 </script>
