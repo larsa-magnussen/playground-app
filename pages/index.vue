f8bcb067bb1a1f20b6b2622f4c913778970c3906
lars <larsa.magnussen@gmail.com>
2024-12-20 10:09:28 +0100

    rmeove extra padding between nav and post list on index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="container mx-auto w-1/2 py-8 divide-y divide-gray-300">
+        <div class="container mx-auto w-1/2 divide-y divide-gray-300">
             <PostItem
                 v-for="post in posts"
                 :post="post"
